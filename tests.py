bd04c4041d862f289085a654a9d776f82e20331e
Anna <annasworkandstudy2025@gmail.com>
2026-03-26 12:23:02 +0500

    Исправление ошибок в тестах и добавление описания в README

--- tests.py
+++ tests.py
@@ -24,22 +24,26 @@ class TestBooksCollector:
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
 
-    @pytest.fixture(autouse=True)
-    def book():
-        book = BooksCollector()
-        book.add_new_book('Гордость и предубеждение и зомби')
-        return book
+    @pytest.fixture
+    def collector(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        return collector
     
-    @pytest.mark.parametrize('book',['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии'])
-    def test_set_book_genre_add_new_genre(self, book, genre):
-        book.set_book_genre(book, genre)
-        assert self.get_book_genre[book]== self.genre
+    
+    @pytest.mark.parametrize('genre',['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии'])
+    def test_set_book_genre_add_new_genre(self, collector, genre):
+        collector = BooksCollector()
+        collector.set_book_genre(collector, genre)
+        assert collector.get_book_genre(collector)== genre
 
-    @pytest.fixture(autouse=True)
-    def test_get_book_genre_show_name_book(self, book):
-        book.set_book_genre(book, 'Ужасы')
-        assert self.get_book_genre(book) == 'Ужасы'
+    
+    def test_get_book_genre_show_name_book(self, collector):
+        collector = BooksCollector()
+        collector.set_book_genre(collector, 'Ужасы')
+        assert collector.get_book_genre(collector) == 'Ужасы'
 
+    
     def test_get_books_with_specific_genre_list_genre_book(self):
         collector = BooksCollector()
         collector.add_new_book('Идиот')
@@ -48,34 +52,38 @@ class TestBooksCollector:
         collector.set_book_genre('Идиот', 'Фантастика')
         collector.set_book_genre('Доктор Айболит', 'Ужасы')
         collector.set_book_genre('Преступление и наказание', 'Фантастика')
-        assert self.books_with_specific_genre('Фантастика')==['Идиот', 'Преступление и наказание']
+        assert collector.get_books_with_specific_genre('Фантастика')==['Идиот', 'Преступление и наказание']
+    
     
     def test_get_books_genre_list_of_genre(self):
         genre_collector = BooksCollector()
         genre_collector.add_new_book('Новая книга')
         genre_collector.set_book_genre('Комедия')
-        assert 'Комедия' in self.books_genre 
+        assert 'Комедия' in genre_collector.get_book_genre() 
 
+    
     def test_get_books_for_children_show_books(self):
         collector = BooksCollector()
         collector.add_new_book('Доктор Айболит')
         collector.set_book_genre('Мультфильмы')
-        assert 'Доктор Айболит' in self.books_for_children
+        assert 'Доктор Айболит' in collector.get_books_for_children()
 
-    @pytest.fixture(autouse=True)
-    def test_add_book_in_favorites_book_is_added(self, book):
-        book.add_book_in_favorites('Гордость и предубеждение и зомби')
-        assert 'Гордость и предубеждение и зомби' in self.favorites
-
-    @pytest.fixture(autouse=True)
-    def test_delete_book_from_favorites_book_is_deleted(self, book):
-        book.add_book_in_favorites('Гордость и предубеждение и зомби')
-        book.delete_book_from_favorites('Гордость и предубеждение и зомби')
-        assert ('Гордость и предубеждение и зомби') not in self.favorites
+     
+    def test_add_book_in_favorites_book_is_added(self, collector):
+        collector = BooksCollector()
+        collector.add_book_in_favorites('Гордость и предубеждение и зомби')
+        assert 'Гордость и предубеждение и зомби' in collector.get_list_of_favorites_books()
     
-    @pytest.fixture(autouse=True)
-    def test_get_list_of_favorites_books(self):
-        
-
-
+    
+    def test_delete_book_from_favorites_book_is_deleted(self, collector):
+        collector = BooksCollector()
+        collector.add_book_in_favorites('Гордость и предубеждение и зомби')
+        collector.delete_book_from_favorites('Гордость и предубеждение и зомби')
+        assert ('Гордость и предубеждение и зомби') not in collector.get_list_of_favorites_books()
+    
+    
+    def test_get_list_of_favorites_books_show_list_favorites(self, collector):
+        collector = BooksCollector()
+        collector.add_book_in_favorites('Гордость и предубеждение и зомби')
+        assert collector.get_list_of_favorites_books() == 'Гордость и предубеждение и зомби'
         
