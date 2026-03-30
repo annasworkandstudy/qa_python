603045a1734269c18765710183b20d16c173f349
Anna <annasworkandstudy2025@gmail.com>
2026-03-30 14:31:10 +0500

    Изменены названия тестов, часть assert и добавлен новый тест test_get_books_for_children_include_age_rating

--- tests.py
+++ tests.py
@@ -31,20 +31,20 @@ class TestBooksCollector:
     
     
     @pytest.mark.parametrize('genre',['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии'])
-    def test_set_book_genre_genre_in_list(self, new_object, genre):
+    def test_set_book_genre_return_genre_in_list(self, new_object, genre):
         book_name='Гордость и предубеждение и зомби'
         new_object.add_new_book(book_name)
         new_object.set_book_genre(book_name, genre)
-        assert new_object.get_book_genre(book_name)== genre
+        assert new_object.books_genre.get(book_name)== genre
 
     
-    def test_get_book_genre_show_genre_book(self, new_object):
+    def test_get_book_genre_return_genre_book(self, new_object):
         new_object.add_new_book('Гордость и предубеждение и зомби')
-        new_object.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
-        assert new_object.books_genre.get('Гордость и предубеждение и зомби') == 'Ужасы'
+        new_object.books_genre['Гордость и предубеждение и зомби'] = 'Ужасы'
+        assert new_object.get_book_genre('Гордость и предубеждение и зомби') == 'Ужасы'
 
     
-    def test_get_books_with_specific_genre_list_name_book(self, new_object):
+    def test_get_books_with_specific_genre_return_list_book(self, new_object):
         new_object.add_new_book('Идиот')
         new_object.add_new_book('Доктор Айболит')
         new_object.add_new_book('Преступление и наказание')
@@ -57,10 +57,10 @@ class TestBooksCollector:
     def test_get_books_genre_list_of_genre(self, new_object):
         new_object.add_new_book('Новая книга')
         new_object.set_book_genre('Новая книга', 'Комедии')
-        assert new_object.books_genre == {'Новая книга': 'Комедии'}
+        assert new_object.get_books_genre() == {'Новая книга': 'Комедии'}
 
     
-    def test_get_books_for_children_show_book(self, new_object):
+    def test_get_books_for_children_return_book(self, new_object):
         new_object.add_new_book('Доктор Айболит')
         new_object.set_book_genre('Доктор Айболит', 'Мультфильмы')
         assert 'Доктор Айболит' in new_object.get_books_for_children()
@@ -72,14 +72,23 @@ class TestBooksCollector:
         assert 'Гордость и предубеждение и зомби' in new_object.get_list_of_favorites_books()
     
     
-    def test_delete_book_from_favorites_book_not_in_favorites(self, new_object):
+    def test_delete_book_from_favorites_removes_book(self, new_object):
         new_object.add_new_book('Гордость и предубеждение и зомби')
         new_object.add_book_in_favorites('Гордость и предубеждение и зомби')
         new_object.delete_book_from_favorites('Гордость и предубеждение и зомби')
         assert ('Гордость и предубеждение и зомби') not in new_object.get_list_of_favorites_books()
     
     
-    def test_get_list_of_favorites_books_book_is_added_in_favourites(self, new_object):
+    def test_get_list_of_favorites_books_is_added_in_favourites(self, new_object):
         new_object.add_new_book('Гордость и предубеждение и зомби')
         new_object.add_book_in_favorites('Гордость и предубеждение и зомби')
-        assert 'Гордость и предубеждение и зомби' in new_object.favorites
+        assert len(new_object.get_list_of_favorites_books()) == 1
+
+
+    def test_get_books_for_children_include_age_rating(self, new_object):
+        new_object.add_new_book('It')
+        new_object.set_book_genre('It', 'Ужасы')
+        new_object.add_new_book('Shrek')
+        new_object.set_book_genre('Shrek', 'Мультфильмы')
+        assert 'Shrek' in new_object.get_books_for_children()
+        assert 'It' not in new_object.get_books_for_children()
